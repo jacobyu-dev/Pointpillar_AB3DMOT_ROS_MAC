2827229f618b81f8f7bcb3b643566e6417f6298d
hrkim <lini1634@naver.com>
2020-09-17 19:34:56 +0900

    catkin_ws/

---
+++ catkin_ws/build/pcl_ld/CMakeFiles/pcl_ld_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_ld_node.dir/src/pcl_ld_node.cpp.o"
+  "/home/mds/catkin_ws/devel/lib/pcl_ld/pcl_ld_node.pdb"
+  "/home/mds/catkin_ws/devel/lib/pcl_ld/pcl_ld_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_ld_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/pcl_ld/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mds/catkin_ws/src/pcl_ld
+# Build directory: /home/mds/catkin_ws/build/pcl_ld
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
